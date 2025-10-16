4b9bb222abbd079d5176f84edeb4a4b2bff0e8fb
dzss3399 <182021396@qq.com>
2025-10-16 22:20:21 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -82,6 +82,7 @@ def update_cloudflare_dns(ip_list, api_token, zone_id, subdomain, domain):
 #        i = i + 1
 #        if  i > 8: 
 #             break
+        print("ip:", ip)
         if check_proxy_ip(ip):     
             response = requests.post(f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records', json=data, headers=headers)
             if response.status_code == 200:
